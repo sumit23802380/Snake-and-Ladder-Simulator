2590171624ab624d11ddac42913705366e2e74a5
sumit23802380 <sumitjangid1692153@gmail.com>
2023-12-02 17:02:09 +0530

    Added : Initialized Repository

---
+++ src/com/bridgelabz/snakeandladdersimulator/SnakeAndLadderMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.snakeandladdersimulator;
+
+/**
+ * @desc: snake and ladder simulator main class
+ */
+public class SnakeAndLadderMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to snake and ladder game");
+    }
+}
